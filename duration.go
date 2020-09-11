4ba12d7ee95b5e1c47ae75c99f0ee1cf805a8177
jfk9w <xp.jfk9w@gmail.com>
2020-09-11 14:08:33 +0300

    duration fixes

---
+++ duration.go
@@ -0,0 +1,32 @@
+package flu
+
+import (
+	"encoding/json"
+	"time"
+
+	"github.com/pkg/errors"
+	yaml "gopkg.in/yaml.v3"
+)
+
+type Duration struct {
+	time.Duration
+}
+
+func (d *Duration) FromString(str string) error {
+	var err error
+	d.Duration, err = time.ParseDuration(str)
+	return err
+}
+
+func (d *Duration) UnmarshalYAML(node *yaml.Node) error {
+	return d.FromString(node.Value)
+}
+
+func (d *Duration) UnmarshalJSON(data []byte) error {
+	var str string
+	if err := json.Unmarshal(data, &str); err != nil {
+		return errors.Wrap(err, "unmarshal string")
+	}
+
+	return d.FromString(str)
+}
--- util.go
+++ util.go
@@ -3,9 +3,14 @@ package flu
 import (
 	"os"
 	"os/signal"
+	"syscall"
 )
 
 func AwaitSignal(signals ...os.Signal) {
+	if len(signals) == 0 {
+		signals = []os.Signal{syscall.SIGINT, syscall.SIGABRT, syscall.SIGKILL, syscall.SIGTERM}
+	}
+
 	c := make(chan os.Signal)
 	signal.Notify(c, signals...)
 	<-c
